d553c7300a82f8895417f0a8c4df45efb5d8ba36
Brian Gershon <briangershon@users.noreply.github.com>
2023-05-06 13:13:27 -0700

    Add initial mint logic. Display missing BTREE allowance as an error for now until feature added.
    Add equity contract ABI.

--- src/Mint.tsx
+++ src/Mint.tsx
@@ -1,36 +1,40 @@
 import { useAccount, usePrepareContractWrite, useContractWrite } from "wagmi";
 import abi from "./abi.json";
 import { goerli, mainnet } from "wagmi/chains";
-import { ethers } from "ethers";
 import { useState } from "react";
 
-const CONTRACT_ADDRESS = "0xc8121e650bd797d8b9dad00227a9a77ef603a84a";
+const CONTRACT_ADDRESS = "0x81Ed0A98c0BD6A75240fD4F65E5e2c43d7b343D9";
 const chainId =
   process.env.REACT_APP_ENABLE_TESTNETS === "true" ? goerli.id : mainnet.id;
 
 console.info(`Contract: ${CONTRACT_ADDRESS}`);
 console.info(`Chain ID: ${chainId}`);
 
-const mintPrice = "0.0250";
+const mintPrice = "1000";
 
 function displayFriendlyError(message: string | undefined): string {
   if (!message) return "";
 
-  if (
-    message.startsWith("insufficient funds for intrinsic transaction cost")
-  ) {
+  if (message.startsWith("insufficient funds for intrinsic transaction cost")) {
     return "insufficient funds for intrinsic transaction cost.";
   }
+
+  if (message.includes("Insufficient allowance")) {
+    return "insufficient allowance. This wallet hasn't granted permissions to the contract to transfer BTREE tokens.";
+  }
+
   return message;
 }
 
 export function Mint() {
+  const [mintCount, setMintcount] = useState(1);
   const [total, setTotal] = useState(mintPrice);
 
-  function calcTotal(donation: string) {
+  function calcTotal(count: string) {
+    setMintcount(parseInt(count, 10));
     const total = (
-      parseFloat(mintPrice) + parseFloat(donation ? donation : "0")
-    ).toFixed(4);
+      parseInt(mintPrice, 10) * parseInt(count ? count : "0", 10)
+    ).toFixed(0);
     setTotal(total);
   }
 
@@ -39,10 +43,9 @@ export function Mint() {
   const { config, error } = usePrepareContractWrite({
     address: CONTRACT_ADDRESS,
     abi,
-    functionName: "mintMembership",
+    functionName: "mint",
     chainId: chainId,
-    args: [address],
-    overrides: { value: ethers.utils.parseEther(total) },
+    args: [0x0, address, mintCount], // 0x0 is BTREE
   });
   const { isLoading, write } = useContractWrite(config);
 
@@ -53,27 +56,36 @@ export function Mint() {
   return (
     <>
       <div className="grid grid-cols-2 gap-6 justify-start font-newtimesroman">
-        <div className="text-right">Minimum Donation:</div>
-        <div className="text-left">{mintPrice} ETH</div>
-        <div className="text-right">Add additional donation:</div>
+        <div className="text-right">Cost per BGOV token:</div>
+        <div className="text-left">{mintPrice} BTREE</div>
+        <div className="text-right">Number of tokens to mint:</div>
         <div className="text-left">
           <input
             className="w-20 input-sm"
             type="number"
-            placeholder="0.00"
+            placeholder="1"
             onChange={(e) => calcTotal(e.target.value)}
-            step="0.01"
-            min="0"
-          />{" "}
-          <span>ETH</span>
+            step="1"
+            min="1"
+          />
         </div>
         <div className="text-right">Total price:</div>
         <div className="text-left">
-          {total} <span>ETH</span>
+          {total} <span>BTREE</span>
         </div>
       </div>
+      <div className="m-4 mt-8 mx-auto max-w-xl font-newtimesroman">
+        <p className="text-xl">What's required for minting?</p>
+        <p className="mt-2">
+          To transfer BTREE tokens, you'll need ETH in your wallet. There will
+          also be two transactions. The first to grant permissions for our
+          contract to transfer BTREE tokens on your behalf, the second to do the
+          transfer and mint your equity tokens.
+        </p>
+      </div>
+
       {error && (
-        <div className="m-4 mx-auto max-w-xl font-newtimesroman">
+        <div className="m-4 mx-auto max-w-xl font-newtimesroman font-bold text-lg text-red-500">
           An error occurred preparing the transaction:{" "}
           {displayFriendlyError(error.message)}
         </div>
